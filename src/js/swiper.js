ae7341343f3094d00cbc42cf2dc65322e06d5398
klyn <klyn@gmail.com>
2024-06-09 21:29:49 -0300

    update

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,27 +2,81 @@ const swiperPrincipal = new Swiper('#swiper-principal', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 1,
+    autoplay: {
+        delay: 5000,
+    },
     pagination: {
         el: '#pagination-principal',
+        clickable: true
     },
+});
+
+const createCustomPagination = (swiperInstance, paginationContainer, perView) => {
+    paginationContainer.innerHTML = '';
+    for (let i = 0; i < swiperInstance.slides.length - (perView - 1); i++) {
+        console.log(swiperInstance.slides.length)
+        const bullet = document.createElement('div');
+        bullet.classList.add('bullet');
+        bullet.addEventListener('click', () => swiperInstance.slideTo(i));
+        paginationContainer.appendChild(bullet);
+    }
+};
+
+const updateCustomPagination = (swiperInstance, paginationContainer) => {
+    const bullets = paginationContainer.querySelectorAll('.bullet');
+    bullets.forEach((bullet, index) => {
+        if (index === swiperInstance.realIndex) {
+            bullet.classList.add('bullet-active');
+        } else {
+            bullet.classList.remove('bullet-active');
+        }
+    });
+};  
+
+const swiperLancamentos = new Swiper('#swiper-lancamentos', {
+    direction: 'horizontal',
+    loop: false,
+    on: {
+        init: function () {
+            createCustomPagination(this, document.querySelector('.custom-pagination-lancamentos'), 5);
+            updateCustomPagination(this, document.querySelector('.custom-pagination-lancamentos'));
+        },
+        slideChange: function () {
+            updateCustomPagination(this,document.querySelector('.custom-pagination-lancamentos'));
+        }
+    },
+    navigation: {
+        nextEl: '#swiper-lancamentos-next',
+        prevEl: '#swiper-lancamentos-prev',
+      },
+    slidesPerView: 5,
+    spaceBetween: 15,
+    autoplay: {
+        delay: 5000,
+    },
+
 });
 
 const swiperBlog = new Swiper('#swiper-blog', {
     direction: 'horizontal',
-    loop: true,
+    loop: false,
+    on: {
+        init: function () {
+            createCustomPagination(this, document.querySelector('.custom-pagination-blog'), 3);
+            updateCustomPagination(this, document.querySelector('.custom-pagination-blog'));
+        },
+        slideChange: function () {
+            updateCustomPagination(this,document.querySelector('.custom-pagination-blog'));
+        }
+    },
+    navigation: {
+        nextEl: '#swiper-blog-next',
+        prevEl: '#swiper-blog-prev',
+      },
     slidesPerView: 3,
     spaceBetween: 15,
-    pagination: {
-        el: '#pagination-blog',
+    autoplay: {
+        delay: 5000,
     },
-});
 
-const swiperLancamentos = new Swiper('#swiper-lancamentos', {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 5,
-    spaceBetween: 50,
-    pagination: {
-        el: '#pagination-lancamentos',
-    },
 });
