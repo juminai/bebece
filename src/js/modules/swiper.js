bd6d268d65238dbca4030aebb304442083503b9d
klyn <klyn@gmail.com>
2024-06-16 13:05:05 -0300

    finalizado?

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -94,7 +94,7 @@ export default function swipers() {
 
     const swiperLancamentos = new Swiper('#swiper-lancamentos', {
         direction: 'horizontal',
-        loop: true,
+        loop: false,
         on: {
             init: function () {
                 createCustomPagination(
